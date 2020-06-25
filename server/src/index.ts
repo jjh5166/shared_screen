16ba6644e408901e7aba36f885f6d03e47733ea6
John <jjh5166@gmail.com>
2020-06-25 19:00:13 -0400

    movieAPI datasource, comment out typeorm

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,28 @@
+require('dotenv').config();
 import "reflect-metadata";
 import { ApolloServer } from 'apollo-server-express';
 import Express from 'express';
-import { createConnection } from 'typeorm';
+// import { createConnection } from 'typeorm';
 
 import { createSchema } from "./utils/createSchema";
+import MovieAPI from './datasources/tmdb';
+declare var process: {
+  env: {
+    DATABASE_NAME: string
+  }
+}
 
 const main = async () => {
-  await createConnection();
-
+  // await createConnection();
   const schema = await createSchema();
+
+  const dataSources = () => ({
+    movieAPI: new MovieAPI(),
+  });
+
   const apolloServer = new ApolloServer({
     schema,
+    dataSources,
     context: ({ req, res }: any) => ({ req, res }),
   })
 
