bc695175646a95148ac7c15452f7319f445fb7ae
John <jjh5166@gmail.com>
2020-07-01 22:30:33 -0400

    SearchPerson on index page

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,27 +1,10 @@
-import Head from 'next/head'
-import { useQuery } from '@apollo/react-hooks';
-import { searchPersonQuery } from '../graphql/searchPerson';
-import { useState } from 'react';
+import Layout from '../components/Layout';
+import SearchPerson from '../components/SearchPerson';
 
-export default function Home() {
-  const [searchTerm, updateSearch] = useState("")
-  const { data } = useQuery(
-    searchPersonQuery,
-    { variables: { searchTerm: searchTerm } },
-  );
+export default () => {
   return (
-    <div className="container">
-      <Head>
-        <title>Shared Screen</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <input type="text" onChange={async (e) => {
-          updateSearch(e.target.value)
-          console.log(data)
-        }} />
-      </main>
-    </div>
+    <Layout>
+      <SearchPerson />
+    </Layout>
   )
 }
