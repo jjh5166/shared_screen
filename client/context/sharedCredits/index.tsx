2fd4b0348ae0ce2008a7152570fe695c4ef333db
John <jjh5166@gmail.com>
2020-07-13 23:17:59 -0400

    sharedState utils for reducer

--- client/context/sharedCredits/index.tsx
+++ client/context/sharedCredits/index.tsx
@@ -1,4 +1,5 @@
-import { createContext, useContext, useReducer } from 'react';
+import { createContext, useContext, useReducer, ReactNode } from 'react';
+
 import removeProperty from '../../utils/removeProperty';
 
 const SharedStateContext = createContext<any>(null);
@@ -8,12 +9,12 @@ type SharedState = {
   shared: number[],
   perPerson: {}
 };
+
 type SharedAction =
   | { type: "ADD"; payload: { id: number, films: Array<number> } }
   | { type: "REMOVE"; payload: number };
 
-
-function setSharedProperty(perPerson: any): Array<any> {
+function setSharedProperty(perPerson: any): Array<number> {
   let values: Array<number> = Object.values(perPerson)
   if (values.length < 2) {
     return []
@@ -27,36 +28,37 @@ function setSharedProperty(perPerson: any): Array<any> {
       }
     }, []
   )
-}
-function setSharedState(state: any, action: any): SharedState {
+};
+
+function setSharedState(perPerson: any): SharedState {
   let newState: SharedState = {
     shared: [],
-    perPerson: {
-      ...state.perPerson,
-      [action.payload.id]: action.payload.films
-    }
+    perPerson: perPerson
   }
   newState.shared = setSharedProperty(newState.perPerson)
   return newState;
-}
+};
+
 function sharedReducer(state: SharedState, action: SharedAction): SharedState {
   switch (action.type) {
     case 'ADD':
-      return setSharedState(state, action)
+      return setSharedState({
+        ...state.perPerson,
+        [action.payload.id]: action.payload.films
+      })
     case 'REMOVE':
-      return {
-        ...state,
-        perPerson: removeProperty(state.perPerson, action.payload)
-      };
+      return setSharedState(removeProperty(state.perPerson, action.payload))
     default:
       return state;
   }
-}
+};
+
 const initialSharedCreditState = {
   shared: [],
   perPerson: {}
 };
-function SharedProvider({ children }: any) {
+
+function SharedProvider({ children }: { children: ReactNode }) {
   const [sharedState, sharedDispatch] = useReducer(sharedReducer, initialSharedCreditState)
   return (
     <SharedStateContext.Provider value={sharedState}>
