c56f3785098a338b1fde71b1834a1f9ed4e9eb6c
John <jjh5166@gmail.com>
2020-07-13 23:18:55 -0400

    show film postrers in Results

--- client/components/Results/index.tsx
+++ client/components/Results/index.tsx
@@ -1,10 +1,31 @@
 import React from 'react';
-import { ResContainer } from "./styled"
 
+import { ResContainer, FilmCardContainer } from "./styled"
+import { useSharedState } from '../../context/sharedCredits';
+import { useCreditsState } from '../../context/credits';
+import { posterImagePath } from '../../utils/posterImagePath';
+
+const FilmCard = ({ film }: any) => (
+  <FilmCardContainer>
+    <img src={posterImagePath(film.posterPath, 185)} />
+  </FilmCardContainer>
+)
 
 const ResultsContainer = React.memo(function ResultsContainer() {
+  const credits = useCreditsState();
+  const sharedFilmIds = useSharedState();
   return (
-    <ResContainer></ResContainer>
+    <ResContainer>
+      {!!sharedFilmIds.shared.length &&
+        sharedFilmIds.shared.map((film: number) => {
+          return (
+            <FilmCard
+              key={film}
+              film={credits[Object.keys(credits)[0]][film]}
+            />
+          )
+        })}
+    </ResContainer>
   )
 })
 
--- client/components/Results/styled.ts
+++ client/components/Results/styled.ts
@@ -4,4 +4,19 @@ export const ResContainer = styled.div`
   height: 80%;
   width: 45%;
   outline: solid 2px green;
+  display: flex;
+  flex-wrap: wrap;
+`
+export const FilmCardContainer = styled.div`
+  display: flex;
+  height: 110px;
+  width: 80px;
+  position: relative;
+  img{
+    max-width: 100%;
+    max-height: 100%;
+    width: auto;
+    display: block;
+    margin: 0 auto;
+  }
 `
