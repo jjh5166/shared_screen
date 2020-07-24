653b00224c5b672eb925e0cbbb04bb183ede2995
John <jjh5166@gmail.com>
2020-07-23 21:21:16 -0400

    separate Suggestion file

--- client/components/Suggestions/index.tsx
+++ client/components/Suggestions/index.tsx
@@ -1,63 +1,10 @@
-import { useApolloClient } from '@apollo/react-hooks';
-
-import { PersonData, Film } from '../../interfaces';
-import { faceImagePath } from '../../utils/faceImagePath';
-import { useSelectedDispatch, useSelectedState } from '../../context/selectedPeople';
-import { fetchCreditsQuery } from '../../graphql/fetchCredits';
-import { useCreditsDispatch } from '../../context/credits';
-import { SuggContainer, SuggCard, ImgSpacer, SuggImgContainer, SuggInfo, NameC } from './styled';
-import { useSharedDispatch } from '../../context/sharedCredits';
-import { Query } from 'react-apollo';
-import { searchPersonQuery } from '../../graphql/searchPerson';
 import { Fragment, useEffect } from 'react';
+import { Query } from 'react-apollo';
 
-const SuggestedPerson = ({ person }: any) => {
-  const selectedDispatch = useSelectedDispatch();
-  const client = useApolloClient();
-  const creditsDispatch = useCreditsDispatch();
-  const sharedDispatch = useSharedDispatch();
-  const selectedPeople = useSelectedState();
-  const clickHandler = async (person: PersonData) => {
-    await selectedDispatch({ type: 'ADD', payload: person })
-    await client.query({
-      query: fetchCreditsQuery,
-      variables: { id: Number(person.id) }
-    }).then(res => {
-      creditsDispatch({
-        type: 'ADD',
-        payload: { id: person.id, info: res.data.fetchCredits.credits }
-      })
-      sharedDispatch({
-        type: 'ADD',
-        payload: { id: person.id, films: res.data.fetchCredits.filmIds }
-      })
-    }).catch(err => console.log(err))
-  }
-  return (
-    <SuggCard
-      tabIndex="0"
-      onClick={async () => {
-        if (!selectedPeople[person.id]) {
-          clickHandler(person);
-        }
-      }}>
-      <ImgSpacer>
-        <SuggImgContainer>
-          <img src={faceImagePath(person.imagePath, 92)} />
-        </SuggImgContainer>
-      </ImgSpacer>
-      <SuggInfo>
-        <NameC><span>{person.name}</span></NameC>
-        <div>
-          {person.knownAs && <span>{person.knownAs}, </span>}
-          {person.knownFor && person.knownFor.map((film: Film, i: number, array: Array<Film>) => (
-            film.title && <span key={film.id}>{i > 0 && array[i - 1].title && ',  '}{film.title}</span>
-          ))}
-        </div>
-      </SuggInfo>
-    </SuggCard>
-  )
-}
+import { PersonData } from '../../interfaces';
+import { SuggContainer } from './styled';
+import { searchPersonQuery } from '../../graphql/searchPerson';
+import SuggestedPerson from './SuggestedPerson';
 
 type SuggestionsProps = { searchTerm: string, displayed: boolean };
 
