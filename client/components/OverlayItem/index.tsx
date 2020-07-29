f1f9df1d8a0eaf3c2ffcc9fbf7c1f7b11195a950
John <jjh5166@gmail.com>
2020-07-29 12:16:19 -0400

    stop prop modal content

--- client/components/OverlayItem/index.tsx
+++ client/components/OverlayItem/index.tsx
@@ -1,13 +1,16 @@
 import { Film } from "../../interfaces";
 import { posterImagePath } from "../../utils/posterImagePath";
-import { PosterContainer } from "./styled";
+import { PosterContainer, ModalContentContainer } from "./styled";
 
 
 export default ({ film }: { film: Film }) => {
 
   return (
-    <PosterContainer>
-      <img src={posterImagePath(film.posterPath, 500)} />
-    </PosterContainer>
+    <ModalContentContainer onClick={(e) => { e.stopPropagation(); }}>
+      <PosterContainer>
+        <img src={posterImagePath(film.posterPath, 500)} />
+      </PosterContainer>
+      <div>{film.overview}</div>
+    </ModalContentContainer>
   )
 }
