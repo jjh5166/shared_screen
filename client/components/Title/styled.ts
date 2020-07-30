4b771d264f876b5e04d246103c2b77b9526bb056
John <jjh5166@gmail.com>
2020-07-30 14:21:19 -0400

    responsive title

--- client/components/Title/styled.ts
+++ client/components/Title/styled.ts
@@ -1,15 +1,32 @@
 import styled from "styled-components";
 import { darken } from "polished";
-import { theme } from "../../constants";
+import { theme, device } from "../../constants";
 
 export const TitleContainer = styled.div`
   h1{
     font-family: 'Brandy';
-    font-size: 4em;
+    font-size: 2.5em;
     color: ${theme.bravo};
-    text-shadow: -4px -2px ${theme.alpha}, -4px -5px ${theme.alpha};
-    -webkit-text-stroke: 1px ${darken(0.2, theme.charlie)};
     paint-order: stroke fill;
     letter-spacing: 0.2rem;
+    text-shadow:
+    -1.5px -1.5px 0 ${darken(0.2, theme.charlie)},
+    1.5px -1.5px 0 ${darken(0.2, theme.charlie)},
+    -1.5px 1.5px 0 ${darken(0.2, theme.charlie)},
+    1.5px 1.5px 0 ${darken(0.2, theme.charlie)},
+    -3px -2px 0 ${theme.alpha},
+    -3px -3px 0 ${theme.alpha};
+    @media ${device.tablet} { 
+      font-size: 5em;
+    }
+    @media ${device.laptop} { 
+      text-shadow:
+        -1.5px -1.5px 0 ${darken(0.2, theme.charlie)},
+        1.5px -1.5px 0 ${darken(0.2, theme.charlie)},
+        -1.5px 1.5px 0 ${darken(0.2, theme.charlie)},
+        1.5px 1.5px 0 ${darken(0.2, theme.charlie)},
+        -4px -2px 0 ${theme.alpha},
+        -4px -5px 0 ${theme.alpha};
+    }
   };
 `
