32300eb57d7bc3ffca5aff3e3cfe35158e7e7000
John <jjh5166@gmail.com>
2020-08-03 16:58:45 -0400

    NoImageOverlay on modal

--- client/components/ModalItem/index.tsx
+++ client/components/ModalItem/index.tsx
@@ -1,6 +1,7 @@
 import { Film } from "../../interfaces";
 import { posterImagePath } from "../../utils/posterImagePath";
 import { PosterContainer, ModalContentContainer, DescriptionContainer } from "./styled";
+import { NoImageOverlay } from "../Card/styled";
 
 
 export default ({ film }: { film: Film }) => {
@@ -9,6 +10,7 @@ export default ({ film }: { film: Film }) => {
     <ModalContentContainer onClick={(e) => { e.stopPropagation(); }}>
       <PosterContainer>
         <img src={posterImagePath(film.posterPath, 342)} />
+        {!film.posterPath && <NoImageOverlay text={film.title} />}
       </PosterContainer>
       <DescriptionContainer>{film.overview}</DescriptionContainer>
     </ModalContentContainer>
