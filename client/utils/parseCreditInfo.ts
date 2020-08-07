2327f559b6fc8363d174765954649ec1a07a21d4
John <jjh5166@gmail.com>
2020-08-06 22:45:31 -0400

    parseInt on credit resolver

--- client/utils/parseCreditInfo.ts
+++ client/utils/parseCreditInfo.ts
@@ -2,7 +2,7 @@ export function parseCreditInfo(arr: Array<any>) {
   return arr.reduce(
     (ob: any, item: any) => {
       let des: Array<any> = Object.values(item)
-      return { ...ob, [des[0]]: des[1] }
+      return { ...ob, [des[0]]: { ...des[1], id: parseInt(des[1].id) } }
     }, {}
   )
 }
