f5c70a6767625c2907b49532816a7e0a8727d146
John <jjh5166@gmail.com>
2020-08-06 21:40:32 -0400

    fetchFilmDetailsQuery

---
+++ client/graphql/fetchFilmDetails.ts
@@ -0,0 +1,17 @@
+import { gql } from "apollo-boost";
+
+export const fetchFilmDetailsQuery = gql`
+  query fetchFilmDetails($id: Int!) {
+	  fetchFilmDetails(id: $id){
+      id,
+      imdbId,
+      rating,
+      genres,
+      castCrew{
+        directedBy,
+        writtenBy
+        cast
+      }
+    }
+  }
+`;
