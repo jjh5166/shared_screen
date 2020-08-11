b4c3cf225229a9e3466295bc6d67aa1d36b89fa9
John <jjh5166@gmail.com>
2020-08-11 17:51:36 -0400

    CastTape, modal styles

--- client/components/ModalItem/index.tsx
+++ client/components/ModalItem/index.tsx
@@ -5,13 +5,18 @@ import { Film } from "../../interfaces";
 import { posterImagePath } from "../../utils/posterImagePath";
 import { NoImageOverlay } from "../Card/styled";
 import { fetchFilmDetailsQuery } from "../../graphql/fetchFilmDetails";
-import { PosterContainer, ModalContentContainer, DescriptionContainer, ModalTop, MovieTitle, UnderTitle } from "./styled";
+import {
+  PosterContainer, ModalContentContainer, DescriptionContainer, ModalTop,
+  MovieTitle, UnderTitle, TapeContainer, CastCredit, ModalBottom
+} from "./styled";
 
 
 export default ({ film }: { film: Film }) => {
   const { data } = useQuery(fetchFilmDetailsQuery, {
     variables: { id: film.id },
   });
+  console.log(data)
+  const TitleAndRelease: string = film.title + (!!film.releaseDate ? ` (${film.releaseDate.slice(0, 4)})` : "");
 
   return (
     <ModalContentContainer onClick={(e) => { e.stopPropagation(); }}>
@@ -20,8 +25,8 @@ export default ({ film }: { film: Film }) => {
           <img src={posterImagePath(film.posterPath, 185)} />
           {!film.posterPath && <NoImageOverlay text={film.title} />}
         </PosterContainer>
-        <MovieTitle>{`${film.title} (${film.releaseDate.slice(0, 4)})`}</MovieTitle>
-        <UnderTitle>{!!data.fetchFilmDetails.genres && data.fetchFilmDetails.genres}</UnderTitle>
+        <MovieTitle>{TitleAndRelease}</MovieTitle>
+        <UnderTitle>{data && !!data.fetchFilmDetails.genres && data.fetchFilmDetails.genres.join(", ")}</UnderTitle>
         <DescriptionContainer>{film.overview}</DescriptionContainer>
       </ModalTop>
       {
@@ -36,10 +41,24 @@ export default ({ film }: { film: Film }) => {
 const MoreFilmDetails = ({ details }: any) => {
   const { castCrew } = details;
   return (
-    <div>
-      <span>Directed By: {castCrew.directedBy}</span><br />
-      <span>Written By: {castCrew.writtenBy}</span><br />
-      <span>Cast: {castCrew.cast}</span><br />
-    </div>
+    <ModalBottom>
+      <span>Directed By:{!!castCrew.directedBy.length ? ` ${castCrew.directedBy.join(", ")}` : "  (uncredited)"}</span><br />
+      <span>Written By:{!!castCrew.writtenBy.length ? ` ${castCrew.writtenBy.join(", ")}` : "  (uncredited)"}</span><br />
+      {!!castCrew.cast && <CastTape cast={castCrew.cast} />}
+    </ModalBottom>
+  )
+}
+const CastTape = ({ cast }: any) => {
+  return (
+    <TapeContainer>
+      {
+        cast.map((member: any, i: number) => (
+          <CastCredit key={member.role + i}>
+            <div>{member.role}</div>
+            <div>{member.name}</div>
+          </CastCredit>
+        )
+        )}
+    </TapeContainer>
   )
 }
