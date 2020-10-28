dd4345d254f0e05d15df707fff43dd04b38d3590
John <jjh5166@gmail.com>
2020-10-28 14:32:15 -0400

    wakeServer query

--- server/src/utils/createSchema.ts
+++ server/src/utils/createSchema.ts
@@ -3,7 +3,8 @@ import { buildSchema } from "type-graphql";
 import { FetchCreditsResolver } from '../modules/person/FetchCreditsResolver';
 import { SearchPersonResolver } from '../modules/person/SearchPersonResolver';
 import { FetchFilmDetailsResolver } from '../modules/film/FetchFilmDetailsResolver';
+import { WakerResolver } from "../modules/waker/WakerResolver";
 
 export const createSchema = () => buildSchema({
-  resolvers: [SearchPersonResolver, FetchCreditsResolver, FetchFilmDetailsResolver],
+  resolvers: [SearchPersonResolver, FetchCreditsResolver, FetchFilmDetailsResolver, WakerResolver],
 });
